5ec50867aa3ad7d837127ea25858c38b85d88685
Venorth Jeyan <venorth.jeyan@hotmail.com>
2025-12-08 17:23:16 -0500

    Increase logo size by 33% while keeping navbar same size

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -39,8 +39,8 @@ export default function Navbar() {
               <Image
                 src={getImage('newlogo')}
                 alt="TurboTech Cleaners Logo"
-                width={98}
-                height={34}
+                width={130}
+                height={45}
                 className="object-contain"
                 priority
               />
