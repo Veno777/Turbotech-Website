60e0159a9a9f0b4ac6fdb74c998b9396e0d5a8a6
Venorth Jeyan <venorth.jeyan@hotmail.com>
2025-12-08 17:12:31 -0500

    Reduce navbar height and padding by 10%

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
       }`}
     >
       <div className="container-custom">
-        <div className="flex items-center justify-between min-h-[110px] py-3">
+        <div className="flex items-center justify-between min-h-[99px] py-2.5">
           {/* Logo - Larger */}
           <div className="flex-shrink-0 overflow-visible">
             <a href="/" className="flex items-center">
